6d33efdd2e038441e81d7b026a2ec5a97318c201
Justin <20047549+justinglotz@users.noreply.github.com>
2025-05-03 10:25:48 -0500

    Update settings.py
    Added a URL to CORS-Whitelist

--- swelocaltours/settings.py
+++ swelocaltours/settings.py
@@ -47,6 +47,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     "https://swe-local-tours-g4rzg2h61-justinglotzs-projects.vercel.app",
+    "https://swe-local-tours-fe.vercel.app"
 )
 
 MIDDLEWARE = [
